6b77c7c3cb9c7837d06e22837e752a12aa2e1578
徐小武 <xuxiaowu.xiaowuxu@bytedance.com>
2024-04-02 17:08:08 +0800

    fix: vconsole导入问题

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import TTShow from '@/pages/ttshow';
 import { configure } from 'axios-hooks';
 import Axios from 'axios';
 import { useEffect } from 'react';
+import { loadScript } from './utils/util';
 console.log('test');
 const axios = Axios.create({
   baseURL: __API_URL__,
@@ -22,11 +23,19 @@ const router = createBrowserRouter([
   },
 ]);
 
+function importVconsole() {
+  const params = new URLSearchParams(window.location.search);
+
+  if (params.get('vconsole') === '1') {
+    loadScript('//unpkg.byted-static.com/vconsole/3.14.6/dist/vconsole.min.js').then(() => {
+      new window.VConsole();
+    });
+  }
+}
+
 function App() {
   useEffect(() => {
-    if (window.VConsole) {
-      new window.VConsole();
-    }
+    importVconsole();
   }, []);
   return <RouterProvider router={router} />;
 }
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -11,3 +11,25 @@ export function formatDateTime(date: Date) {
 function pad(num: number) {
   return num.toString().padStart(2, '0');
 }
+
+export function loadScript(url: string) {
+  return new Promise<void>(resolve => {
+    const script: any = document.createElement('script');
+    script.type = 'text/javascript';
+
+    if (script.readyState) {
+      script.onreadystatechange = function () {
+        if (script.readyState === 'loaded' || script.readyState === 'complete') {
+          script.onreadystatechange = null;
+          resolve();
+        }
+      };
+    } else {
+      script.onload = function () {
+        resolve();
+      };
+    }
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  });
+}
