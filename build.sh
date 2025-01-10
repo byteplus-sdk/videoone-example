0eeb8ed6c6056da5775e901ea823bade6b5e08d3
xuxiaowu.xiaowuxu <xuxiaowu.xiaowuxu@bytedance.com>
2025-01-10 18:51:31 +0800

    fix: 报错修复

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ set -e
 
 # 切换node版本
 source /etc/profile
-nvm install 18
-nvm use 18
+# nvm install 18
+# nvm use 18
 
 node -v
 npm -v
-npm i -g pnpm@7.16
+npm i -g pnpm@5.5
 pnpm -v
 pnpm install
 pnpm build
