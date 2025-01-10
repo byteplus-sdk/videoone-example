302cade3922e13be6dcca9fb22259db65c21f834
xuxiaowu.xiaowuxu <xuxiaowu.xiaowuxu@bytedance.com>
2025-01-10 19:51:52 +0800

    fix: userid丢失

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 # 切换node版本
 source /etc/profile
-# nvm install 18
-# nvm use 18
+nvm install 18
+nvm use 18
 
 node -v
 npm -v
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import flexible from '@/utils/flexible.ts';
 
 flexible(window, document);
 
+window.sessionStorage.setItem('user_id', String(Math.floor(10000 + Math.random() * 90000)));
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
