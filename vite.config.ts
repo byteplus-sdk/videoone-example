72319f198cdadd656565b5b1d218a19d8298aae7
xuxiaowu.xiaowuxu <xuxiaowu.xiaowuxu@bytedance.com>
2025-02-06 18:48:34 +0800

    fix: 警告

--- vite.config.ts
+++ vite.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
         }),
         autoprefixer({
           overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ie >= 8', 'last 2 versions'],
-          grid: true,
+          grid: false,
         }),
       ],
     },
